a74fe3d6af670778f948746a9dc2439b41ad4fd5
adamskoog <adamskoog@outlook.com>
2015-02-27 15:44:06 -0600

    1. Optimize code by retrieving the thumbnail from the youtube url in each feed item instead of making a request to each individual page. 2. No longer using the rss feed library, using xml and parsing with xpath to get some of the items that were not available from feedparser. 3. Modularize some of the code to keep it a bit cleaner and add error/sanity checking.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,6 +5,8 @@ NAME 	 = "TekThing Podcast"
 ART      = 'art-default.jpg'
 ICON     = 'icon-default.png'
 RSSFEED  = 'http://feeds.feedburner.com/tekthing'
+YT_START = 'embed/'
+YT_END = '?wmode'
 # ################################################
 
 # This function is initially called by the PMS framework to initialize the plugin. This includes
@@ -17,28 +19,66 @@ def Start():
 @handler(PREFIX, NAME, thumb=ICON, art=ART)
 def MainMenu():
 	oc = ObjectContainer()
-	xml = RSS.FeedFromURL(RSSFEED)				# Info on RSS API: https://dev.plexapp.com/docs/api/parsekit.html#module-RSS
-	for item in xml.entries:					# Use FeedParser to get the "items", in this case episodes (/rss/channel/item): https://pythonhosted.org/feedparser/reference-entry.html
-	
-		# Pull the data that is available in the form of link, title, date and description. Use get to check if element exists: https://pythonhosted.org/feedparser/basic-existence.html
-		title = item.get('title', NAME)												# Get the video title
-		date = Datetime.ParseDate(item.get('pubDate', ''))							# Get the date the video is released
-		url = item.get('link', '')													# Get the link to the video page
-		desc = item.get('description', '')											# Get the episode summary
+	xml = XML.ElementFromURL(RSSFEED)
+
+	for item in xml.xpath('//item'):
+		title = item.xpath('./title/text()')[0]
+		thumb = GetThumbUrl(item)
+		url = GetVideoUrl(item)
 		
-		Log('Feed item found: ' + title)
-		
-		oc.add(VideoClipObject(
-			url = url, 
-			title = title, 
-			summary = desc, 
-			thumb = R(ICON),						# Use generic thumb for time being, Feed doesn't give a thumb and site pages don't have suitable image.
-			originally_available_at = date
+		if url:
+			url = item.xpath('./link/text()')[0]	# Use page url, need tp pass to service to get summary on video page.
+
+			oc.add(VideoClipObject(
+				url = url, 
+				title = title, 
+				thumb = thumb
+				)
 			)
-		)
 		
 	# Display empty ObjectContainer and write to log.
 	if len(oc) < 1:
-		Log ('No items found in Feed')
+		Log.Debug('No items found in Feed')
 		return ObjectContainer(header="Empty", message="No Videos found for this Show right now.")     
 	return oc
+
+# Unfortunately the episode summary is unreliable at best. Maybe after a few more
+# episodes they will finalize their rss feed.
+def GetEpisodeSummary(item):
+	itunesSummary = item.xpath('./*[local-name()="summary"]/text()')
+	if len(itunesSummary) > 0:
+		return itunesSummary[0]
+	description = item.xpath('./description/text()')
+	if len(description) > 0 and 'iframe' not in description[0]:
+		Log.Debug('description: ' + description[0])
+		return description[0]
+	return 'No Summary Available'
+
+# See if we can pull the thumbnail from the RSS feed via the youtube url.
+def GetThumbUrl(item):
+	content = item.xpath('./*[local-name()="encoded"]/text()')
+	if len(content) > 0:
+		youtubeurl = FindYoutubeUrl(content[0])
+		if youtubeurl:
+			Log.Debug('youtube url 1: ' + youtubeurl)
+			return youtubeurl
+	content = item.xpath('./description/text()')
+	if len(content) > 0:
+		youtubeurl = FindYoutubeUrl(content[0])
+		if youtubeurl:
+			Log.Debug('youtube url 2: ' + youtubeurl)
+			return youtubeurl
+	Log.Debug('no content found')
+	return R(ICON)
+
+def GetVideoUrl(item):
+	# Need to research how to do this without service.
+	enclosure = item.xpath('./enclosure')
+	if len(enclosure) > 0:
+		return enclosure[0].get('url')
+	return ''
+
+def FindYoutubeUrl(content):
+	startIndex = content.find(YT_START) + len(YT_START)
+	endIndex = content.find(YT_END)
+	return 'http://img.youtube.com/vi/' + content[startIndex:endIndex] + '/0.jpg'
